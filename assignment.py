d40d2ec7a3b2573ca4cf3055b46fdfd51413ad8b
Michael Joseph <michaeljoseph195@gmail.com>
2022-08-23 00:09:44 +0000

    working version 2

--- assignment.py
+++ assignment.py
@@ -1,5 +1,6 @@
 from dateutil.parser import parse
 from termcolor import colored as color
+from .cfg import SUBJECT_NAME_TRUNCLEN
 
 class Assignment():
     def __init__(self, props):
@@ -11,8 +12,17 @@ class Assignment():
         date = props['date']
         self.datetime = parse(f'{date} {time}')
 
+    def truncate_name(self):
+        if self.subject == 'Strjál stærðfræði I':
+            self.name = self.name[:self.name.find('("')]
+        if len(self.name) > SUBJECT_NAME_TRUNCLEN:
+            self.name = self.name[:SUBJECT_NAME_TRUNCLEN] + '...'
+        return self.name
+
+
     def __str__(self):
         date = color(self.datetime.strftime('%d-%m-%Y'), 'yellow')
         time = color(self.datetime.strftime('%H:%M'), 'blue')
         notes = color(f'({self.notes})', 'grey') if self.notes else ''
+        self.truncate_name()
         return f'{date} {time} | {self.subject:30} | {self.name} {notes}'
